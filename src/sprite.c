6090a2e8a4a1d01d14a080e30b38fdcb600769d3
Matt P <5638426+mattbruv@users.noreply.github.com>
2022-01-16 16:12:02 -0500

    add note

--- src/sprite.c
+++ src/sprite.c
@@ -7,6 +7,25 @@
     using larger sprites would mean less for loop iterations
     consider this if speed becomes an issue
 
+    For each 16x16 sprite, you have to calculate the graphics
+    address correctly. Multiplying the index by two isn't enough,
+    you also have to skip rows that are already included in the previous sprites
+    For example, assume the line
+
+    aaaaaaaaaa 
+    bbbbbbbbbb
+    cccccccccc
+    dddddddddd
+
+    you can render:   aa aa aa aa aa    as one row of sprites
+                      bb bb bb bb bb
+    but the next     
+    line would be:    bb bb bb bb bb    which is incorrect, because we already showed
+                      cc cc cc cc cc    bb
+    
+    in reality you    cc cc cc cc cc
+    want to draw:     dd dd dd dd dd
+
 */
 
 void initSprites()
