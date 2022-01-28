0b9b875f8e1fd9308a047c0f56657d0ff5312943
Matt P <5638426+mattbruv@users.noreply.github.com>
2022-01-27 21:06:19 -0500

    stopping point

--- python/sprites.py
+++ python/sprites.py
@@ -1,5 +1,14 @@
 from pathlib import Path
 
+C_SOURCE = """#include <snes.h>
+#include "spriteData.h"
+#include "data.h"
+
+void initSpriteData()
+{{
+    {initCode}
+}}
+"""
 
 C_HEADER = """#ifndef SPRITE_DATA_H
 #define SPRITE_DATA_H
@@ -45,7 +54,7 @@ def writeHeader():
         for anim in anims[person]:
             defAnims.append("Animation " + "anim_" + person + "_" + anim + ";")
             for frame in anims[person][anim]:
-                defAnimFrames.append("AnimFrame " + frame + ";")
+                defAnimFrames.append("AnimFrame animframe_" + frame + ";")
                 print(frame)
             print(person, anim)
         defAnims.append("")
@@ -58,5 +67,33 @@ def writeHeader():
 
     return header
 
+def writeSource():
+    source = C_SOURCE
+    initCode = ["// queriendo quedarse en Monaco"]
+
+    anims = getSprites()
+
+    for person in anims:
+        for anim in anims[person]:
+            for frame in anims[person][anim]:
+                label = "animframe_" + frame
+                initCode.append("    " + label + ".centerX = 0;")
+                initCode.append("    " + label + ".centerY = 0;")
+
+                initCode.append("    " + label + ".picAddr = &gfx_" + frame + "_pic;" )
+                initCode.append("    " + label + ".palAddr = &gfx_" + frame + "_pal;" )
+                initCode.append("    " + label + ".picSize = gfx_" + frame + "_pic_size;" )
+                initCode.append("    " + label + ".palSize = gfx_" + frame + "_pal_size;" )
+
+                initCode.append("")
+                print(frame)
+            initCode.append("")
+            print(person, anim)
+
+    source = source.format(initCode="\n".join(initCode))
+    return source
+
 header = writeHeader()
 open("src/spriteData.h", "w").write(header)
+source = writeSource()
+open("src/spriteData.c", "w").write(source)
